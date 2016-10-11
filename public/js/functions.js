07a93faa0517718faf88b866ffe427febc6dd48f
Guilherme Iobbi <guilherme.iobbi@gmail.com>
2016-10-11 16:48:27 -0300

    'custo' values updated

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,6 +1,10 @@
 $(function () {
   $("[data-toggle='tooltip']").tooltip();
 
+  $('#btnNewGame').click(function() {
+      window.location.reload(false);
+  });
+
   socket = io();
   $('form').submit(function(){
       var idCarta = $(this).attr('id');
@@ -14,10 +18,15 @@ $(function () {
         for (var item in data) {
           var id = '#'+item;
           $(id).text(data[item]);
+
           if(item == 'myCastle' || item == 'enemyCastle' || item == 'myWall' || item == 'enemyWall') {
             var percent = $(id).width() / $(id).parent().width() * 100;
             $(id).width(percent - (percent - data[item]) + '%');
           }
+        }
+        // TODO: Colocar a verificacao em um listener (doc ready)
+        if($('#myCastle').text() >= 100 || $('#enemyCastle').text() <= 0) {
+          $('#winModal').modal();
         }
       }
   });
